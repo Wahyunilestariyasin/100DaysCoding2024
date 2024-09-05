8452d6e45595a6286165799148136d35a36afcc9
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-05 21:02:21 +0800

    Hari4
    Membuat perintah cetak menggunakan sout print dan Sout printf

---
+++ CodingDay4.java
@@ -0,0 +1,15 @@
+package CodingDay4;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("Biodata singkat");
+        
+        System.out.printf("%nnama saya %s %numur saya %d","Wahyuni",19);
+        
+        
+        
+    }
+    
+}
