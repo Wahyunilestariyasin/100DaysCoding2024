6e7b7d0198fc76ec0aa41a023eaf275a65211983
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-06 15:23:33 +0800

    Hari5
    Membuat komentar dalam codingan

---
+++ CodingDay5.java
@@ -0,0 +1,15 @@
+package CodingDay5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Komentar
+        /*Ini adalah komentar yang panjang *mau jadi orang sukses*/
+        /*tahap pertama kesuksesan adalah gabung di ISC*/
+         
+        
+        
+        
+    }
+    
+}
