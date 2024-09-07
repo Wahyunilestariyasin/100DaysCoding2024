7304a3cfe3f4901558a279be9c00cae69790797f
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-07 15:56:02 +0800

    Create CodingDay6.java
    Membuat Deklarasi variabel

---
+++ CodingDay6.java
@@ -0,0 +1,17 @@
+package CodingDay6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String Bungou;
+        int Stray;
+        byte dogs;
+        double adalah;
+        char anime;
+        boolean yang;
+        float saya;
+        long rekomendasikan;
+        
+    }
+    
+}
