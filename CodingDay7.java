e973c64ae79db35b55b145c4a6dd1f413a6e13d2
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-08 18:25:49 +0800

    Hari7
    Inisialisasi variabel

---
+++ CodingDay7.java
@@ -0,0 +1,16 @@
+package CodingDay7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String am="budi";
+        int am_am=1000;
+        double am_am_am=41.0;
+        char am_am_am_am='$';
+        boolean am_am_am_am_am=false;
+        System.out.printf(" nama saya = %s %n punya pohon cabe= %d %n %.1f ukuran sepatu %n lambang mata uang kesukaan=%c %n apa semua infonya benar? %b",am,am_am,am_am_am,am_am_am_am,am_am_am_am_am);
+        
+        
+    }
+    
+}
