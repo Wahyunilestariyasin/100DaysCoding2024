e458930c67656f036a80e7e47b93bd754af8cfc3
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-09 20:33:44 +0800

    Hari8
    Update variabel + assigment

---
+++ CodingDay8.java
@@ -0,0 +1,19 @@
+package CodingDay8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String warna_kulit;
+        /*sebelum update*/
+        warna_kulit=" putih";
+        //update
+        warna_kulit =" sawo matang";
+        System.out.println("warna kulit" +  warna_kulit);
+        //Assigment
+        String nama_presiden ="megawati";
+       nama_presiden += " soekarno puteri";
+       System.out.println("presiden perempuan RI :" +nama_presiden);
+       
+    }
+    
+}
