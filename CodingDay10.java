1ba1ba6c55a7cfaa6a1d921624eb2de44f31c1a4
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-11 21:03:01 +0800

    Hari10
    Membuat codingan menggunakan Tipe data byte

---
+++ CodingDay10.java
@@ -0,0 +1,14 @@
+package CodingDay10;
+
+ public class Main {
+     public static void main(String[]args) {
+         byte awal= -128;
+         byte akhir= 127;
+         System.out.println(" daya tampung byte ");
+         System.out.print( awal + " sampai ");
+         System.out.print( akhir);
+         
+         
+         
+     }
+ }
