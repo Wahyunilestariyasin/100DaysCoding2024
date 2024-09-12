87f98e7c854f6f8e4980a6959a439014a45ca702
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-12 19:56:48 +0800

    Hari11
    Membuat Variabel menggunakan tipe data short

---
+++ CodingDay011.java
@@ -0,0 +1,13 @@
+package CodingDay11;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        short pertama = -32768;
+        short terakhir = 32767;
+        
+        System.out.println("mulai dari angka " + pertama);
+        System.out.println("sampai angka " + terakhir);
+    }   
+    
+}
