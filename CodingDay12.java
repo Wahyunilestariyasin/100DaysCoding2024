a3fdc417d1f376b75146c0da51b817e22b9afd36
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-13 20:29:40 +0800

    Hari12
    Membuat deklarasi variabel menggunakan tipe data integer(int)

---
+++ CodingDay12.java
@@ -0,0 +1,13 @@
+package CodingDay12;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int jumlahAwal=-2345678;
+        int jumlahAkhir=2345678;
+        
+        System.out.println("hitungan angka "+jumlahAwal);
+        System.out.println("hitungan angka "+jumlahAkhir);
+    }
+    
+}
