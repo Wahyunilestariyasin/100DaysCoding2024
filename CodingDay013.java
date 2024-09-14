3690961b64efc5edc88ab8e1c0d015ea49b91c7f
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-14 10:40:00 +0800

    Hari13
    Evaluasi mentor

---
+++ CodingDay013.java
@@ -0,0 +1,25 @@
+//Soal kedua
+package eva1;
+//Soal Ketiga
+public class Wahyuni {
+    //Soal ke empat
+    public static void main (String []args){
+        //soal kelima
+        System.out.println("Wahyuni Lestari Yasin");
+        
+        //soal keenam
+        String nama = "wahyuni lestari yasin";
+        String alamat ="kalukku";
+        int angkatan = 2024;
+        
+        System.out.println("nama\t:"+nama);
+        System.out.println("alamat\t:"+alamat);
+        System.out.println("angkatan:"+angkatan);
+        
+        
+
+        
+     
+}
+    
+}
