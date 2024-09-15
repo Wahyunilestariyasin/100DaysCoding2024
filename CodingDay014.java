795d25807e6232b34229793535b5ef4714e02667
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-15 20:20:54 +0800

    Hari14
    Membuat deklarasi Variabel dengan tipe data long

--- CodingDay014.java
+++ CodingDay014.java
@@ -1,1 +1,16 @@
+package CodingDay14;
 
+public class Main {
+    
+    public static void main(String[] args) {
+        /*Variabel long*/
+     long a,b;
+        a = -876543221;
+        b = 876543112;
+        
+        System.out.println("long bisa di isi "+ a);
+        System.out.println("juga bisa berisi nilai "+b);
+        
+    }
+    
+}
