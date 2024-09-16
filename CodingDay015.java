47d239e80b7f09eb84524f8d29aea441ebc16878
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-16 20:15:12 +0800

    Hari15
    Membuat deklarasi variabel menggunakan tipe data double

---
+++ CodingDay015.java
@@ -0,0 +1,12 @@
+package Hari14;
+
+public class CodingDay15 {
+    public static void main(String [] args){
+        double a = 16.9;
+        double b = 20.03;
+        
+        System.out.println("tanggal hari ini :" + a);
+        System.out.println("jam ngoding \t :"+ b);
+    }
+    
+}
