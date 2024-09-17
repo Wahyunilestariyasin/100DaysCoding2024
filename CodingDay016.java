c55d60b11702bbbff8040ee929f2c88073250013
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-17 20:39:10 +0800

    Create CodingDay016.java
    Membuat deklarasi variabel menggunakan tipe data float

---
+++ CodingDay016.java
@@ -0,0 +1,18 @@
+package CodingDay16;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //tipe data float
+        
+        float a,b;
+        a = -4566.788f;
+        b = 887.6654f;
+        
+        System.out.println("angka min :"+ a);
+        System.out.println("angka\t  :"+b);
+        
+        
+    }
+    
+}
