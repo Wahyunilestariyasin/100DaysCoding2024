9496fcb354a0d172b88574069efdbd54f41bf1d3
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-18 21:53:47 +0800

    Create CodingDay017.java
    Membuat deklarasi variabel menggunakan tipe data Char

---
+++ CodingDay017.java
@@ -0,0 +1,13 @@
+package CodingDay17;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        char A1 = '¥';
+        char A2 = '$';
+        
+        System.out.println("mata uang jepang :"+A1);
+        System.out.println("lambang dolar  \t:"+A2);
+    }
+    
+}
