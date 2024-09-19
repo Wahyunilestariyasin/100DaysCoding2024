81ac937eb2dfb675079e62934d1e128cf0503f02
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-19 21:51:19 +0800

    Hari18
    Membuat deklarasi variabel dengan tipe data boolean

---
+++ CodingDay018.java
@@ -0,0 +1,13 @@
+package codingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        boolean ben3r = true;
+        System.out.println("coding itu seru? :" + ben3r);
+        
+        
+    }
+    
+}
