136a0f5fd0211e339d3a3ab910052796a34495ea
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-20 23:00:45 +0800

    Hari19
    Membuat deklarasi variabel menggunakan tipe data Reference String

---
+++ CodingDay019.java
@@ -0,0 +1,14 @@
+package CodingDay19;
+public class Main {
+    
+    public static void main(String[] args) {
+        String judul ="coding is fun :";
+        String chapter1 = "hari yang cerah ";
+        String chapter2 = "untuk ngoding";
+        
+        System.out.println(judul);
+        System.out.println(chapter1+chapter2);
+        
+    }
+    
+}
