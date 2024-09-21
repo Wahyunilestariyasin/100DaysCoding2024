001b64000e6731bcdf178de9e119275dc9c28212
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-21 08:44:27 +0800

    Hari20
    Membuat deklarasi variabel menggunakan tipe data Byte reference

---
+++ CodingDay020.java
@@ -0,0 +1,20 @@
+package CodingDay20;;
+
+public class hari20 {
+    public static void main(String[]args){
+        Byte a = null;
+        Byte b = 15;
+        //Byte reference bisa bernilai angka dan null(tidak ada nilai)
+        //Byte reference atau non primitif berawal huruf kapital
+        System.out.println("nilai a :" +a);
+        System.out.println("nilai b :" +b);
+        byte c =30;
+        byte d =c;
+        //tidak dapat berisi null dan selalu memiliki nilai
+        
+        System.out.println("nilai c :"+ c);
+        System.out.println("nilai d :"+ d);
+        
+    
+}
+}
