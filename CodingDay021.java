b98d96fcd579f43979422ad829da982d98722510
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-22 08:56:23 +0800

    Hari21
    Membuat deklarasi variabel menggunakan tipe data reference Short

---
+++ CodingDay021.java
@@ -0,0 +1,25 @@
+package CodingDay;
+
+public class CodingDay21 {
+    public static void main (String[]args){
+        //short non primitif/reference
+        
+        Short nonPrimitif= 130;
+        System.out.println("non primitif\t:"+ nonPrimitif);
+        
+        //short primitif 
+        
+        short primitif = 100;
+        System.out.println("primitif\t:"+ primitif);
+        
+        /*perbedaannya antara lain:
+         * non primitif disebut short objek yang memiliki metode tambahan sedangkan yang primitif tidak 
+         * penggunaan memori lebih banyak dari primitif 
+         * non primitif menggunakan huruf kapital dihuruf pertamanya
+         * nilai defaultnya 
+         * 
+         */
+            
+    }
+    
+}
