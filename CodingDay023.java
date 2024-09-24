836e3d44800bfc575ecb6f38a26e01d066a57d24
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-24 21:44:59 +0800

    Hari23
    Membuat deklarasi variabel menggunakan tipe data Long Reference

---
+++ CodingDay023.java
@@ -0,0 +1,11 @@
+package CodindDay;
+
+public class CodingDay23 {
+    public static void main (String []args){
+        Long nilai = 350000L;
+        
+        System.out.println("nilai long " + nilai);
+        
+    }
+    
+}
