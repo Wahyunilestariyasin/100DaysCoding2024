6ca8934671b0ea9270d8c6ac37bf930a7c39a821
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-25 20:24:59 +0800

    Hari24
    Membuat deklarasi variabel menggunakan tipe data reference Float

---
+++ CodingDay024.java
@@ -0,0 +1,14 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Float tinggi = 149f;
+        Float Berat =43f;
+        
+        System.out.println("Tinggi badan saya :" +tinggi+ " cm");
+        System.out.println("Berat badan saya  :" +Berat+ " kg");
+        
+    }
+    
+}
