3bef04638c14b5f1b7021b8c2d1b22cff60e4bd9
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-26 22:45:13 +0800

    Create CodingDay025.java
    Membuat deklarasi variabel menggunakan tipe data reference Double

---
+++ CodingDay025.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Double x= 267.7;
+        Double y= 22.31;
+        
+        System.out.println("nilai Double hari :" +x);
+        System.out.println("nilai Double ini:" +y);
+    }
+    
+}
