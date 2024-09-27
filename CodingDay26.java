50c8df64a13777af99e8cc641fefcc5492968727
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-27 19:57:43 +0800

    Create CodingDay26.java
    Membuat deklarasi variabel menggunakan tipe data reference Character

---
+++ CodingDay26.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Character a ='*';
+        Character b ='$';
+        
+        System.out.println("karakter 1 :"+a);
+        System.out.println("karakter 2 :"+b);
+    }
+    
+}
