b180a9f863ebb7faf3a5b77b3aa3d131289836a4
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-29 23:20:38 +0800

    Hari28
    Membuat deklarasi variabel menggunakan tipe data reference Boolean

---
+++ Codingday028.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Boolean a = true;
+        Boolean b = false;
+        
+        System.out.println("dapat 0 di ICL?\t"+a);
+        System.out.println("Mau menyerah?\t"+b);
+    }
+    
+}
