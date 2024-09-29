bc48f648167b2bdb46c9669331fa64f303b4f385
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-29 23:14:36 +0800

    Hari27
    Codingan pelengkap 100 days 
    Hari evaluasi

---
+++ CodingDay027.java
@@ -0,0 +1,13 @@
+package codingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String a = "Melihat dari hasil kemarin,aku harus belajar";
+        
+        System.out.println(a);
+        
+        
+    }
+    
+}
