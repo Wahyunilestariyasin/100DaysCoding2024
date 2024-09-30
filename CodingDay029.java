3bf8373ef1813d6b515c047fabc86019dab8c41e
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-09-30 20:13:29 +0800

    Hari29
    Menggunakan tipe data reference bigInteger

---
+++ CodingDay029.java
@@ -0,0 +1,14 @@
+package CodingDay;
+import java.math.BigInteger;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger ("12345678910111213141516178");
+        BigInteger b = new BigInteger("253637282832828282929209988");
+        
+        System.out.println("kasih sayang anak ke orangtua ="+a);
+        System.out.println("cinta orangtua ke anak" + "=" +a+b);
+    }
+    
+}
