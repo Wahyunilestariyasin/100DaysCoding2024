f5bdf74686c51cd82a13af279397aa18ab0415f2
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-02 02:31:50 +0800

    Hari30
    Membuat deklarasi variabel menggunakan tipe data reference BigDecimal

---
+++ CodingDay030.java
@@ -0,0 +1,13 @@
+package CodingDay;
+import java.math.BigDecimal;
+
+public class Main {
+    
+public static void main(String[] args) {
+       
+BigDecimal a = new BigDecimal("150000000");
+        
+         System.out.println("penghasilan yang saya inginkan perbulan : RP."+ a);
+    }
+    
+}
