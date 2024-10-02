6dda54d96427f35a3ad91e6c759d060d12f8f4e7
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-03 03:21:22 +0800

    Create CodingDay031.java
    Membuat konversi string ke integer

---
+++ CodingDay031.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String jarakkota ="256";
+        
+        int jarak =Integer.parseInt(jarakkota);
+        
+        System.out.println("jarak antara kota A dan B = "+jarak+"km");
+    }
+    
+}
