cfe1500e293fa75400245d12197961c7db38666f
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-03 11:04:15 +0800

    Hari32
    Membuat konversi tipe data primitif ke String

---
+++ CodingDay032.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        float a = 25.4f;
+        String V =String.valueOf(a);
+        
+        System.out.println(a);
+        System.out.println(V);
+    }
+    
+}
