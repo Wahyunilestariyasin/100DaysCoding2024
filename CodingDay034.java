1aac394553f73b1046769269282bd5e94f96530a
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-05 21:26:34 +0800

    Create CodingDay034.java
    Input melalui keyboard dengan Scanner

---
+++ CodingDay034.java
@@ -0,0 +1,19 @@
+package CodingDay;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan angka pertama :");
+        int a = sc.nextInt();
+        
+        System.out.print("masukkan angka kedua :");
+        int b = sc.nextInt();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        
+    }
+    
+}
