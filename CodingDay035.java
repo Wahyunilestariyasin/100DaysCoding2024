04100b583620c44cfda0abddc45993b1194204eb
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-06 10:35:01 +0800

    Hari 35
    Operator aritmatika penjumlahan dan pengurangan pada pemrograman java

---
+++ CodingDay035.java
@@ -0,0 +1,12 @@
+package CodingDay;
+
+public class CodingDay35 {
+    public static void main(String[]args){
+        int a =50;
+        int b =17;
+        
+        System.out.println("hasil penjumlahan a+b :"+(a+b));
+        System.out.println("hasil pengurangan a-b :"+(a-b));
+    }
+    
+}
