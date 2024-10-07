f624eda6d1ff998443252a47a75a9ade65b8a8af
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-07 19:36:41 +0800

    Hari36
    Menggunakan operator aritmatika perkalian,pembagian,dan modulus.

---
+++ CodingDay036.java
@@ -0,0 +1,14 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a =15;
+        int b =5;
+        
+        System.out.println("hasil perkalian :"+a*b);
+        System.out.println("hasil pembagian :"+a/b);
+        System.out.println("hasil modulus   :"+a%b);
+    }
+    
+}
