b344abec3184627bfc04f2b1b50bff5272450214
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-08 19:01:33 +0800

    Hari37
    Membuat program dengan menggunakan operator penugasan penambahan dan pengurangan

---
+++ CodingDay037.java
@@ -0,0 +1,18 @@
+package CodingDay;
+
+public class Main{
+    public static void main(String[]args){
+        
+        int w;
+        
+        w =10;//pengisian nilai
+        
+        w+=5;//penugasan penambahan
+        System.out.println("penugasan += "+w);
+        
+        w =10;
+        w-=5;//penugasan pengurangan
+        System.out.println("penugasan -= "+w);
+    }
+    
+}
