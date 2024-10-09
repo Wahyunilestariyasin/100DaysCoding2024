129a1ecd18699de2e33892095dd22bdfd88fe1e4
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-10 06:44:59 +0800

    Hari39
    Menggunakan operator penugasan 
    Pengisian dan modulus

---
+++ CodingDay039.java
@@ -0,0 +1,22 @@
+package CodingDay;
+
+public class CodingDay39{
+    public static void main(String...args){
+        
+        int a;
+        
+        //pengisian dan modulus
+        
+        a=8;
+        a%=2;
+        
+        System.out.println("hasil modulus: "+a);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
