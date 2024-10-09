0e9347f2f7124a64a64350a48814b232a7dfa717
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-10 05:51:14 +0800

    Hari 38
    Menggunakan operator penugasan perkalian dan pembagian

---
+++ CodingDay038.java
@@ -0,0 +1,22 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        int a = 10;
+        
+        // Pengisian dan Perkalian (a *= b 
+        a *= 5;
+        System.out.println("Hasil Pengisian dan Perkalian a *= b: " + a);
+        
+        
+        double c = 21;
+        
+        
+        // Pengisian dan Pembagian c /= d
+        c /= 4;
+        System.out.println("Hasil Pengisian dan Pembagian c /= d: " + c);
+    }
+}
