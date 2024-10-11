11c577209d897727de97153e422e8b582d77b26b
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-11 08:56:31 +0800

    Create CodingDay040.java
    Menggunakan operator perbandingan lebih besar dan lebih kecil

---
+++ CodingDay040.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class CodingDay40 {
+    public static void main(String...args){
+        int a =100;
+        int b =20;
+        // perbandingan lebih besar a>b
+        System.out.println("perbandingan lebih besar (a>b):"+(a>b));
+        // perbandingan lebih kecil a<b
+        System.out.println("perbandingan lebih kecil (a<b):"+(a<b));
+    }
+    
+}
