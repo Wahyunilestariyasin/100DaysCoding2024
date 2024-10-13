134779c9e51a6559c82438d87e68ed679d8825c8
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-13 21:32:41 +0800

    Hari42
    Menggunakan operator perbandingan sama dengan (==) dan tidak sama dengan(!=)

---
+++ CodingDay42.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class CodingDay42 {
+    public static void main(String[]args){
+        int a,b;
+        a=12;
+        b=10;
+        
+        System.out.println("apakah a sama dengan b :"+(a==b));
+        System.out.println("apakah a tidak sama dengan b :"+(a!=b));
+    }
+    
+}
