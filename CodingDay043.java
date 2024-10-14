ebc40fbbd55b01aec93de528f62b221799db5dad
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-14 22:13:03 +0800

    Hari43
    Menggunakan operator lebih besar sama dengan dan lebih kecil sama dengan

---
+++ CodingDay043.java
@@ -0,0 +1,15 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte a =12;
+        
+        boolean hasil=(a>=12);
+        boolean hasil2=(a<=10);
+        System.out.println(">= :"+hasil);
+        System.out.println("<= :"+hasil2);
+        
+    }
+    
+}
