53044dc5fdce0edf0c6c7f45195c5411616437ab
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-16 23:04:21 +0800

    Hari45
    Menggunakan operator OR (||)

---
+++ CodingDay045.java
@@ -0,0 +1,18 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int a = 12;
+        
+       boolean hasil=(a>10 || a<10);
+        
+        System.out.println("hasilnya :"+hasil);
+        
+        //syarat Logika OR
+        //jika salah satunya bernilai true maka outputnya true
+        //dan akan bernilai false ketika kedua kondisinya bernilai false
+    }
+    
+}
