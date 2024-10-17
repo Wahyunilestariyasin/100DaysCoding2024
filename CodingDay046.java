aaa5795ce00ed21f778afc97e0cbb94076530ccb
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-17 16:51:52 +0800

    Hari46
    Menggunakan operator logika negasi

---
+++ CodingDay046.java
@@ -0,0 +1,15 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //menggunakan negasi
+        boolean semangka=false;
+        boolean pepaya =true;
+        System.out.println("apakah aku suka semangka? :"+  ! semangka);
+        System.out.println("apakah kamu suka pepaya? :" + ! pepaya);
+        
+        //negasi akan menampilkan kebalikan dari kondisi
+    }
+    
+}
