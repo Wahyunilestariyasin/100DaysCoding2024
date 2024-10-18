832d9f95c81dcdba061b1e92b1da88d31fd310da
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-18 20:56:26 +0800

    Hari47
    Menggunakan percabangan if (hanya 1 kondisi)

---
+++ CodingDay047.java
@@ -0,0 +1,15 @@
+package codingday;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        boolean a= 12<(10*2);
+        
+        if (a==true){
+            System.out.println("Easy peazy lemon squeze");
+        
+        }
+        
+    }
+    
+}
