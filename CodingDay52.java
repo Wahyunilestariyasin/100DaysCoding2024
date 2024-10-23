98c226bbe686b6a65cb46e6608bca416e443cab8
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-23 23:07:51 +0800

    Hari52
    Menggunakan ternary dalam program

---
+++ CodingDay52.java
@@ -0,0 +1,11 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a =10;
+       String hasil = (a<=20)?"real":"fake";
+        System.out.println("apakah hasilnya :"+hasil);
+    }
+    
+}
