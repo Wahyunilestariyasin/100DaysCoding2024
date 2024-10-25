cdd537d858ee3bf336e87fd7f132c15f7606bf51
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-25 21:51:02 +0800

    Hari54
    menggunakan looping for increment

---
+++ CodingDay54.java
@@ -0,0 +1,12 @@
+package Codingday;
+
+public class Main {
+    
+    public static void main(String[] args){
+        for (int i = 1; i <= 200; i++) {
+            System.out.println("Aku cinta Ngoding");
+        }
+    }
+
+    }
+
