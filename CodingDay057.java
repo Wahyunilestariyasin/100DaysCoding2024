3bda53f70a85f6a3ce3ba83866138ba9023f5f83
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-28 23:52:30 +0800

    Update CodingDay057.java

--- CodingDay057.java
+++ CodingDay057.java
@@ -1,1 +1,14 @@
-
+package CodingDay;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        int jumlah=1;
+        while(jumlah<=15){
+            System.out.println("hitungan ke "+jumlah);
+            jumlah++;
+            
+        }
+    }
+    
+}
