2923d54f781431061b07678f51c21ca6618ae6f5
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-10-30 23:12:56 +0800

    Hari 59
    Menggunakan For each

---
+++ CodingDay059.java
@@ -0,0 +1,12 @@
+package CodingDay;
+import java.util.Arrays;
+public class Main {
+    
+    public static void main(String[] args) {
+        int [] arr = {7,6,9,5,11,4};
+        
+        for(int hitung : arr){
+        System.out.println("angka ke :"+hitung);
+    }
+}
+}
