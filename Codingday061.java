a86b8b030c07fe4632abec9a479835fae04662d5
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-01 23:23:40 +0800

    Hari61
    Membuat perulangan tak terhingga

---
+++ Codingday061.java
@@ -0,0 +1,11 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("aku sayang mamaku");
+        }
+    }
+    
+}
