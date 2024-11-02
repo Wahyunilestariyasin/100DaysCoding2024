617c99c9d8c75803a8697ce8de22e089c8305b64
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-02 22:08:13 +0800

    Hari62
    Break kedalam loop

---
+++ CodingDay062.java
@@ -0,0 +1,15 @@
+package CodingDay;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        for(int awal=1;awal<=100;awal++){
+            System.out.println(awal);
+            if (awal==50){
+                break;
+            }
+        }
+        
+    }
+    
+}
