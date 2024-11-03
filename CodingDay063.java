55695546e9c730f284d73c562052b20f87e92da8
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-03 21:06:23 +0800

    Hari63
    Menggunakan continue didalam looping

---
+++ CodingDay063.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for(int angka =1; angka<=50 ; angka++){
+            if(angka%2==1){
+                continue;
+            }System.out.println(angka);
+        }
+    }
+    
+}
