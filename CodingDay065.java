455b445ab9f6640cf43d2a352a234e12fb3516a7
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-05 22:59:05 +0800

    Hari 65
    Nested loop (perulangan didalam perulangan)

---
+++ CodingDay065.java
@@ -0,0 +1,16 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for(int a = 1;a<=3;a++){
+            System.out.println();
+            System.out.print(a+" ");
+            for(int b =1;b<=3;b++){
+                System.out.print(b);
+            }
+            
+        }
+        
+            }
+}
