29405a7540b1e2ecaa7407a1ab151a2ea56443e5
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-06 22:26:54 +0800

    Create CodingDay066.java
    Membuat pola persegi dan persegi panjang

---
+++ CodingDay066.java
@@ -0,0 +1,26 @@
+package CodingDay;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("persegi :");
+        for(int i = 1 ; i<=4; i++){
+            System.out.print("\n*");
+            for(int p=1;p<=8; p++){
+                System.out.print("*");
+            }
+        }
+        
+        
+        
+        System.out.println("\npersegi panjang :");
+        for(int a=1;a<=4;a++){
+          System.out.print("\n*");
+            for(int b =1;b<=16;b++){
+                System.out.print("*");
+            }
+            }
+           
+    }
+    
+}
