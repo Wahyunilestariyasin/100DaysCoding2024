813ef3ac99866d853c3ab0c3ff5495da820430fa
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-08 00:07:08 +0800

    Hari67
    Method void

---
+++ CodingDay67.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class Main {
+    public static void main(String[] args) {
+        Main mainhehe = new Main();
+        
+        mainhehe.greetings();
+    }
+    
+    public void greetings() {
+        System.out.println("Assalamualaikum");
+    }
+}
