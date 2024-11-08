7765f4d07d6ce1993e20794bbdd187094c4badc8
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-08 22:49:38 +0800

    Hari 68

---
+++ Coding Day068.java
@@ -0,0 +1,17 @@
+package codingDay;
+
+public class Nonvoid68 {
+    public static int aku(int a ){
+        return a+2;
+        
+        
+    }
+    
+    public static void main(String[]args){
+        
+        int hasil = aku(8);
+        System.out.println(hasil);
+        
+    }
+    
+}
