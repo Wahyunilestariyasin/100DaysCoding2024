081b48c9278720a15e8f54793e88572d92415776
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-11 20:45:33 +0800

    Hari71
    Method Static

---
+++ CodingDay071.java
@@ -0,0 +1,11 @@
+package CodingDay;
+
+public class Day71 {
+    public static void aduh() {
+        System.out.println("tolong kaki saya sakit");
+    }
+    public static void main(String[]args){
+    
+    Day71.aduh();
+    }
+}
