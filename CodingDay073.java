b4b0a3544c221f79ed3cec15f5f61b524dbe9a43
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-13 21:40:31 +0800

    Hari73
    Method char

---
+++ CodingDay073.java
@@ -0,0 +1,15 @@
+package Codingday;
+
+public class methodCharAt73 {
+    public static void main(String[]args){
+        String kalimat = "CodingIsFun";
+        char print = kalimat.charAt(0);
+        char print2 =kalimat.charAt(8);
+        
+        //method char mengambil nilai didalam variabel String
+        //dengan index pertama dimulai dari 0
+        System.out.println("ini huruf index 0 = "+print);
+        System.out.print("ini huruf index 8 = "+print2);
+    }
+    
+}
