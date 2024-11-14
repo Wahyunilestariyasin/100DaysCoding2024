a792fd668aa6692d58fc1066bc1a36cf1703ecb0
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-15 01:43:47 +0800

    Hari74
    String method length

---
+++ Codingday074.java
@@ -0,0 +1,13 @@
+package CodingDay;
+
+public class StringMethodLength {
+    public static void main(String[]args){
+        String kalimat ="good question";
+        int inilength = kalimat.length();
+        System.out.println("hasilnya :"+inilength);
+        
+        
+        
+    }
+    
+}
