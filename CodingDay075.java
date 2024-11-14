daba44c92a375422678ae6374f70b7b7a27c5aed
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-15 01:52:44 +0800

    Hari75
    Method toLowerCase

---
+++ CodingDay075.java
@@ -0,0 +1,9 @@
+package CodingDay;
+
+public class methodToLowerCase75 {
+    public static void main(String[]args){
+        String sebelum ="GaS AJAsIH";
+        String sesudah =sebelum.toLowerCase();
+        System.out.println("hasil :"+sesudah);
+    }
+}
