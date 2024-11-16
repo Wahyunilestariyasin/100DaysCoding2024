d08e8a99b24beab45a8e593844dd921f9586799a
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-16 23:19:57 +0800

    Hari 76
    Method to uppercase

---
+++ CodingDay076.java
@@ -0,0 +1,11 @@
+package codingDay;
+
+public class toUpperCase76 {
+    public static void main(String[]args){
+        String a1="inFO MAbar";
+        String a2=a1.toUpperCase();
+        
+        System.out.println(a2);
+    }
+    
+}
