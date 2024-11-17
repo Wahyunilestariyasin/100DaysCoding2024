b5a235ae314441f8fab5480e408a00dae8e1c3f3
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-17 19:24:10 +0800

    Hari77
    String method:equals();

---
+++ CodingDay077.java
@@ -0,0 +1,16 @@
+package CodingDay;
+
+public class equals77 {
+    public static void main(String[] args){
+    
+    String pertama = "aduh kaki saya sakit";
+    String kedua = "tolong kaki saya sakit";
+    
+    if (pertama.equals(kedua)){
+        System.out.println("+10000Aura");
+    }else{
+        System.out.println("Skibidi");
+    }
+    
+}
+}
