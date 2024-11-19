c30de902065f79b07dbadba0e3f171d754e53a5f
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-19 21:09:46 +0800

    Create CodingDay079.java
    To String

---
+++ CodingDay079.java
@@ -0,0 +1,11 @@
+package CodingDay;
+
+public class toString79 {
+    public static void main(String[]args){
+        int a =13;
+        String b =Integer.toString(a);
+        
+        System.out.println(b);
+    }
+    
+}
