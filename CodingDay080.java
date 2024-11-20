138fc6a340b6ff01b2ee8642d2c268bbd24aac13
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-20 20:49:01 +0800

    Hari80
    String method trim

---
+++ CodingDay080.java
@@ -0,0 +1,13 @@
+package codingDay;
+
+public class metodetrim80 {
+    public static void main(String[] args){
+        String aduh = "  yabaii  ";
+        String ehek = aduh.trim();
+        
+        System.out.println("sebelum :"+aduh);
+        System.out.println("sesudah :"+ehek);
+    }
+    
+    
+}
