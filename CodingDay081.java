e8b38487d8f565fecac433d89492d4ba5afa2e33
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-21 21:41:15 +0800

    Hari81
    String methode valueOf();

---
+++ CodingDay081.java
@@ -0,0 +1,15 @@
+package CodingDay;
+
+public class stringMethod81 {
+    public static void main(String[]args){
+        int a =12345;
+        double b=67891.0;
+        
+        String c = String.valueOf(a);
+        String d = String.valueOf(b);
+        
+        System.out.println("ini sout variabel C:"+c);
+        System.out.println("ini sout variabek D:"+d);
+    }
+    
+}
