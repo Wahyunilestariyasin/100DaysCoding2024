ada9e15e79d71865c3dceea298085ba18ce8e624
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-22 21:30:00 +0800

    Hari 82
    String Method ceil,floor,round

---
+++ CodingDay082.java
@@ -0,0 +1,15 @@
+package CodingDay;
+public class ceil_floor_round_82 {
+    public static void main(String[]args){
+        double hehe = 3.5;
+        double hihi =2.2;
+        
+        System.out.println("ini hasil ceil haha :"+Math.ceil(hehe));
+        System.out.println("ini hasil ceil hihi :"+Math.ceil(hihi));
+        System.out.println("\nini hasil floor haha :"+Math.floor(hehe));
+        System.out.println("ini hasil floor hihi :"+Math.floor(hihi));
+        System.out.println("\nini hasil round haha :"+Math.round(hehe));
+        System.out.println("ini hasil round hihi :"+Math.round(hihi));
+    }
+    
+}
