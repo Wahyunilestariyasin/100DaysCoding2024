144187f001ac0940ea4c7b5aaebbc35ac1fd8337
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-25 21:59:47 +0800

    Create CodingDay085.java
    Menginisialisasi array 1 dimensi

---
+++ CodingDay085.java
@@ -0,0 +1,10 @@
+package CodingDay85;
+
+public class inisialisasiArray1D {
+    public static void main(String[]args){
+        //inisialisasi array satu dimensi
+        String hewan [] = {"kucing","ayam","anjing","ular","capibara"};
+        
+    }
+    
+}
