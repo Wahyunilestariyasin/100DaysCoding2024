de0e6db997a57fd497b5c8519b6812fe5fb1316a
Wahyunilestariyasin <wahyunilestariyasin@gmail.com>
2024-11-26 21:14:46 +0800

    Hari86
    Mencetak elemen Array1D

---
+++ CodingDay086.java
@@ -0,0 +1,9 @@
+package CodingDay;
+
+public class cetakArray1D{
+    public static void main(String[]args){
+        String hewan [] = {"kucing","ayam","anjing","ular","capibara"};
+        System.out.println("elemen arraynya adalah :"+hewan[0]+","+hewan[1]+","+hewan[2]+","+hewan[3]+","+hewan[4]);
+    }
+    
+}
